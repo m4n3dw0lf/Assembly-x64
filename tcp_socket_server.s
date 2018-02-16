27bc66f6696beba98097bc8b184ff32a0298d9d4
m4n3dw0lf <m4n3dw0lf@gmail.com>
2018-02-15 23:53:06 -0200

    TCP Socket Server

--- tcp_socket_server.s
+++ tcp_socket_server.s
@@ -7,7 +7,7 @@ section .text
   global _start
     _start:
         call main
-	call exit
+        call exit
     main:
         push rbp
         mov rbp,rsp
@@ -18,9 +18,9 @@ section .text
         syscall ; socket
         mov [s], rax
         mov word [server], 0x2
-        mov ebx , 0xfeffff80 ; 254.255.255.128 = inverse of 127.0.0.1
-        not ebx ; noted to become 127.0.0.1 and prevent null bytes
-        mov dword [server+4], ebx ; 127.0.0.1
+        mov ebx, 0xfeffff80
+        not ebx ; "noted" 127.0.0.1
+        mov dword [server+4], ebx
         mov word [port], 0x5c11 ; 4444
         movzx rax, word[port]
         mov word [server+2], ax
@@ -28,14 +28,17 @@ section .text
         mov rdx, 0x10
         lea rsi, [server]
         mov rdi, rax
-        mov rax, 42
-        syscall ; connect
-	mov rax, 0
-	pop rbp
-	ret
-
+        mov rax, 49
+        syscall ; bind
+        pop rsi
+        push 50
+        pop rax
+        syscall ; listen
+        push 43
+        pop rax
+        syscall ; accept
+        ret
     exit:
-	mov    rax, 60
-	mov    rdi, 0
-	syscall
-
+        mov    rax, 60
+        mov    rdi, 0
+        syscall
