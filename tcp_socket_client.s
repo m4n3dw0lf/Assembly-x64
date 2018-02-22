3681c49ae7c48159acdde38278b47faf78741bff
m4n3dw0lf <m4n3dw0lf@gmail.com>
2018-02-21 22:41:09 -0300

    TCP Client and Server exchanging messages.

--- tcp_socket_client.s
+++ tcp_socket_client.s
@@ -1,8 +1,13 @@
+section .data
+  request db "Hello Server!",0xa
+  len_request equ $-request
+  response db ''
 section .bss
-  s resb 8
+  socket resb 8
   buf resb 32
   port resb 16
   server resb 16
+  saved resb 8
 section .text
   global _start
     _start:
@@ -15,25 +20,37 @@ section .text
         mov rsi, 1
         mov rdi, 2
         mov rax, 41
-        syscall ; socket
-        mov [s], rax
+        syscall ; sys_socket
+        mov [socket], rax
         mov word [server], 0x2
-        mov ebx , 0xfeffff80 ; 254.255.255.128 = inverse of 127.0.0.1
-        not ebx ; noted to become 127.0.0.1 and prevent null bytes
-        mov dword [server+4], ebx ; 127.0.0.1
+        mov ebx, 0xfeffff80 ; inverse of 127.0.0.1
+        not ebx ; noted to become 127.0.0.1
+        mov dword [server+4], ebx
         mov word [port], 0x5c11 ; 4444
         movzx rax, word[port]
         mov word [server+2], ax
-        mov rax, [s]
+        mov rax, [socket]
         mov rdx, 0x10
         lea rsi, [server]
         mov rdi, rax
         mov rax, 42
-        syscall ; connect
-	mov rax, 0
-	pop rbp
+        syscall ; sys_connect
+        mov rax, 1
+        mov rdi, [socket] ; write destination = socket
+        mov rdx, len_request
+        mov rsi, request
+	syscall ; sys_write
+        mov rax, 0
+        mov rdi, [socket] ; read destination = socket
+        mov rdx, 14
+        mov rsi, response
+        syscall ; sys_read
+        mov rax, 1
+        mov rdi, 1 ; write destination != socket
+        syscall ; sys_write
+        mov rax, 0
+        pop rbp
 	ret
-
     exit:
 	mov    rax, 60
 	mov    rdi, 0
--- tcp_socket_server.s
+++ tcp_socket_server.s
@@ -1,8 +1,12 @@
+section .data
+  response db 'Hello Client!',0xa
+  len_response equ $-response
+  buf db ''
 section .bss
-  s resb 8
-  buf resb 32
+  socket resb 8
   port resb 16
   server resb 16
+  client resb 16
 section .text
   global _start
     _start:
@@ -15,30 +19,48 @@ section .text
         mov rsi, 1
         mov rdi, 2
         mov rax, 41
-        syscall ; socket
-        mov [s], rax
+        syscall ; sys_socket
+        mov [socket], rax
         mov word [server], 0x2
-        mov ebx, 0xfeffff80
-        not ebx ; "noted" 127.0.0.1
+        mov ebx, 0xfeffff80 ; noted 127.0.0.1
+        not ebx
         mov dword [server+4], ebx
         mov word [port], 0x5c11 ; 4444
         movzx rax, word[port]
         mov word [server+2], ax
-        mov rax, [s]
+        mov rax, [socket]
         mov rdx, 0x10
-        lea rsi, [server]
+        lea rsi, [server] ; load server structure
         mov rdi, rax
-        mov rax, 49
-        syscall ; bind
+        mov [client], rdi
+        push 49
+        pop rax
+        syscall ; sys_bind
         pop rsi
         push 50
         pop rax
-        syscall ; listen
-        push 43
-        pop rax
-        syscall ; accept
-        ret
+        syscall ; sys_listen
+        mov rax, 43
+        syscall ; sys_accept
+        mov rdi, rax
+        mov r8d, eax
+        mov rax, 0
+        mov rdx, 14
+        mov rsi, buf
+        syscall ; sys_read
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, buf
+        mov rdx, 14
+        syscall ; sys_write (local)
+        mov rsi, 0
+        mov rax, 1
+        mov rdi, r8
+        mov rsi, response
+        mov rdx, len_response
+        syscall ; sys_write (remote)
     exit:
         mov    rax, 60
         mov    rdi, 0
         syscall
+
